1a33ec0c0b59b6df6cb2aa7441c97b7999a7e989
GasparMir <20193tn015@utez.edu.mx>
2025-05-27 15:50:39 -0600

    [First Commit] Inicializacion de proyecto

---
+++ src/main/java/utez/edu/mx/mljg9b/Mljg9bApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.mljg9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mljg9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mljg9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/mljg9b/Mljg9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.mljg9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mljg9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
